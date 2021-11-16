a4a66c9da30c4fe73f6cfb1df891d86e4ac5a5a4
Kristian Vassilev <kvasilev@devrix.com>
2021-11-16 16:17:13 +0200

    Main functionality and semple style is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [result, setResult] = useState("");
+
+	const clickHandler = (e) => {
+		setResult(result.concat(e.target.name));
+	}
+	const clearHandler = () => {
+		setResult("");
+	}
+	const backspaceHandler = () => {
+		setResult(result.slice(0, -1));
+	}
+	const resultHandler = () => {
+		try{
+			setResult(eval(result).toString());
+		}catch(err){
+			setResult("Error");
+		}
+	}
+
+	return (
+		<>
+			<div className="container">
+				<form>
+					<input type="text" value={result} />
+				</form>
+				<div className="keypad">
+					<button className="hightlight" id="clear" onClick={clearHandler}>Clear</button>
+					<button className="hightlight" id="backspace" onClick={backspaceHandler}>C</button>
+					<button className="hightlight" name="/" onClick={clickHandler}>&divide;</button>
+					<button name="7" onClick={clickHandler}>7</button>
+					<button name="8" onClick={clickHandler}>8</button>
+					<button name="9" onClick={clickHandler}>9</button>
+					<button className="hightlight" name="*" onClick={clickHandler}>&times;</button>
+					<button name="4" onClick={clickHandler}>4</button>
+					<button name="5" onClick={clickHandler}>5</button>
+					<button name="6" onClick={clickHandler}>6</button>
+					<button className="hightlight" name="-" onClick={clickHandler}>&ndash;</button>
+					<button name="1" onClick={clickHandler}>1</button>
+					<button name="2" onClick={clickHandler}>2</button>
+					<button name="3" onClick={clickHandler}>3</button>
+					<button className="hightlight" name="+" onClick={clickHandler}>+</button>
+					<button name="0" onClick={clickHandler}>0</button>
+					<button name="." onClick={clickHandler}>.</button>
+					<button className="hightlight" id="result" onClick={resultHandler}>=</button>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default App;
